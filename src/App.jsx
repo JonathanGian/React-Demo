e020df1e539f94b942babc47305e6b2e8f847510
Jonathan Gian <153126165+JonathanGian@users.noreply.github.com>
2024-11-04 14:33:18 +0200

    Remove past weeks code from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { useState } from "react";
 import JobCard/* Jobcard can be changed to anything(just a name for the thing you are calling) */ from "./JobCard"
 import personsData from './Data/personsData';
+
 function Card(props) {
   return (
     <p className="card">
@@ -11,18 +12,8 @@ function Card(props) {
   );
 };
 
-
-
-
-
-
 function App() {
-/*   const [person, setPerson] = useState({
-    firstName: "Margit",
-    title: "Developer",
-    age: 52,
-    animal: "Cat"
-  }); */
+
   const [isLoggedin, setIsLoggedIn] = useState(false);
 
   const handleLoginClick = () => {
@@ -45,37 +36,5 @@ function App() {
   )
 }
 
-/* return (
-
-  <div className='page'>
-    <header>Logo</header>
-    <main>
-      {personsData.map((person) =>
-        <JobCard
-          key = {person.id}
-          name = {person.firstName}
-          title = {person.title}
-          age = {person.age}
-          animal = {person.animal}
-        />)} */
-
-{/* 
-      <JobCard name={person.firstName} title={person.title} age={person.age} animal={person.animal}/>
-      <JobCard name= "Katarina" title= "Developer" age= "25"/>
-      <JobCard name= "Maria" title= "Designer" age= "55"/>
-      <JobCard name= "Markus" title= "Worker" age= "34"/>
-      <JobCard name= "Jimmy" title= "Artist" age= "21"/>
-      <JobCard name= "Bob" title= "Intern" age= "65"/> */}
-/*       </main>
-    </div> */
-
-/*     <main>
-    <Card firstname= "Jonathan" lastname = "Gian" message= "This will be the first message"/>
-    <Card firstname = "Dave" lastname = "Bush" message = "Second message!"/>
-    <Card firstname = "Bob" lastname = "Pääkkönen" message = "Third message!"/>
-    <Card/>
-    </main> */
-/*   )
-} */
 
 export default App
